f3d0accdb7d01cba79bf06effb4fe093218d9028
Lawrence <lawrencehome21@gmail.com>
2023-07-17 01:56:55 -0700

    added a superscript number on top of the cart whose functionality will be implemented later

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
                             <a class="nav-link" href="#">Contact</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#"><i class="fa-solid fa-cart-shopping"></i></a>
+                            <a class="nav-link" href="#"><i class="fa-solid fa-cart-shopping"></i><sup>1</sup></a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Total Price:100/-</a>
