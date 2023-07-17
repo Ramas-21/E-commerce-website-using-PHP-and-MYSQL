71cd3cf3459b79b92d47dd3a7b4b24fc2f172d86
Lawrence <lawrencehome21@gmail.com>
2023-07-17 01:34:29 -0700

    created the div container-fluid for the navbar

--- index.php
+++ index.php
@@ -11,10 +11,11 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     </head>
     <body>
-        
-
-
-
+        <!--------- navbar -------->
+        <div class="container-fluid">
+            <!------first child---->
+            
+        </div>
 
 
     <!------- Bootstrap js link ------>
