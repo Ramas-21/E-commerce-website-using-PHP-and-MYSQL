48c417699c8acf601d20f3350ab815c9f4edacb2
Lawrence <lawrencehome21@gmail.com>
2023-07-17 01:42:31 -0700

    added some bootstrap classes

--- index.php
+++ index.php
@@ -14,9 +14,9 @@
 
 <body>
     <!--------- navbar -------->
-    <div class="container-fluid">
+    <div class="container-fluid p-0">
         <!------first child---->
-        <nav class="navbar navbar-expand-lg bg-light">
+        <nav class="navbar navbar-expand-lg bg-info">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">Navbar</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
