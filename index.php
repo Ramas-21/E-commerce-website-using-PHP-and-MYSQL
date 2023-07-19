515ccdfe75a6bae71b17a4bab812ec6775e7ea2f
Lawrence <lawrencehome21@gmail.com>
2023-07-19 10:39:17 -0700

    added an <a> on the welcome bar

--- index.php
+++ index.php
@@ -52,12 +52,14 @@
             </div>
         </nav>
 
+
+        
         <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
             <ul class="navbar-nav me-auto">
                 <li class="nav-item">
                     <a href="#" class="nav-link">Welcome Guest</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item"></li>
                     <a href="#" class="nav-link">Login</a>
                 </li>
             </ul>
