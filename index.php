a51eab12e95d014b5ed6a26a4ec80ca7d9a25221
Lawrence <lawrencehome21@gmail.com>
2023-07-19 10:13:45 -0700

    added a login <a> on the welcome guest navbar

--- index.php
+++ index.php
@@ -57,6 +57,9 @@
                 <li class="nav-item">
                     <a href="#" class="nav-link">Welcome Guest</a>
                 </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Login</a>
+                </li>
             </ul>
         </nav>
 
