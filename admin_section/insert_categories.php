24f901a6f86d3a4d02ac30273b4986204da70e9f
Lawrence <lawrencehome21@gmail.com>
2023-07-22 10:03:15 -0700

    included the connection file in the insert_categories file

--- admin_section/insert_categories.php
+++ admin_section/insert_categories.php
@@ -1,3 +1,12 @@
+<?php
+include('../includes/connect.php');
+
+
+
+?>
+
+
+
 <form action="#" method="post" class="mb-2">
     <div class="input-group w-90 mb-2">
         <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
