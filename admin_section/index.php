95edcaafaff7acd4f56f0c3d80ae70a7ba092878
Lawrence <lawrencehome21@gmail.com>
2023-07-24 06:13:36 -0700

    modified margin

--- admin_section/index.php
+++ admin_section/index.php
@@ -65,7 +65,7 @@
         </div>
 
 
-        <div class="container my-5">
+        <div class="container my-3">
             <?php
             if(isset($_GET['insert_category'])){
                 include('insert_categories.php');
