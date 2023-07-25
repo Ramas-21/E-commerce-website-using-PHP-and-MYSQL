c97c2d919514cb770a82ffba8b39c96eb4b47262
Lawrence <lawrencehome21@gmail.com>
2023-07-25 04:03:11 -0700

    created a h1 header insert products

--- admin_section/insert_products.php
+++ admin_section/insert_products.php
@@ -8,7 +8,10 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="style.css">
   </head>
-  <body>
-
+  <body class="bg-light">
+    <div class="container mt-3">
+        <h1 class="text-center">Insert products</h1>
+        <form action="#" method="post" enctype="multipart/form-data"></form>
+    </div>
   </body>
 </html>
