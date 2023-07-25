af2d165f21d7cd446cb31d0502b0b906232d9fee
Lawrence <lawrencehome21@gmail.com>
2023-07-25 04:16:13 -0700

    created a form with an input field

--- admin_section/insert_products.php
+++ admin_section/insert_products.php
@@ -1,17 +1,25 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Insert Products</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="style.css">
-  </head>
-  <body class="bg-light">
+</head>
+
+<body class="bg-light">
     <div class="container mt-3">
         <h1 class="text-center">Insert products</h1>
-        <form action="#" method="post" enctype="multipart/form-data"></form>
+        <form action="#" method="post" enctype="multipart/form-data">
+            <div class="form-outline mb-4 w-50 m-auto">
+                <label for="product_title" class="form-label">Product Name</label>
+                <input type="text" name="product_name" id="product_title" class="form-control" placeholder="Enter product name" autocomplete="off" required>
+            </div>
+        </form>
     </div>
-  </body>
+</body>
+
 </html>
