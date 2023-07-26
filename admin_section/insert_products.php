433d256424c966494b8a4b63d0f123fd81946aad
Lawrence <lawrencehome21@gmail.com>
2023-07-26 05:52:00 -0700

    extracted all the category data in the database

--- admin_section/insert_products.php
+++ admin_section/insert_products.php
@@ -39,7 +39,8 @@ include('../includes/connect.php');
                     $select_query = "SELECT * FROM `categories`";
                     $result_query = mysqli_query($connect,$select_query);
                     while($row = mysqli_fetch_assoc($result_query)){
-                        
+                        $category_title = $row['category'];
+                        $category_id = $row['category_id'];
                     }
                     ?>
                     <option value="#">category1</option>
