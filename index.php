4e553c1ee1f00bb5e5d027f56e19516dd45975cd
Lawrence <lawrencehome21@gmail.com>
2023-08-10 16:07:35 -0700

    included the alt var to be displayed in case of any error

--- index.php
+++ index.php
@@ -102,7 +102,7 @@ include('includes/connect.php');
                         */
                         echo "<div class='col-md-4 mb-2'>
                         <div class='card'>
-                            <img src='./images/istockphoto-1132357230-1024x1024.jpg' class='card-img-top' alt='...'>
+                            <img src='./admin_section/product_images/$product_image1' class='card-img-top' alt='$product_name'>
                             <div class='card-body'>
                                 <h5 class='card-title'>$product_name</h5>
                                 <p class='card-text'>$product_description</p>
