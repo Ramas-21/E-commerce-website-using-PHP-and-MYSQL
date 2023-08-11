a228cc90fea43dc86d4ab018bb319db0c8dec190
Lawrence <lawrencehome21@gmail.com>
2023-08-10 21:01:47 -0700

    changed var into camelCase

--- functions/functionsToUse.php
+++ functions/functionsToUse.php
@@ -1,29 +1,30 @@
 <?php
 include('./includes/connect.php');
-function getProducts()
-{
-    global $connect;
-    $select_query = "SELECT * FROM `products` order by rand() LIMIT 0,9";
-    $result_query = mysqli_query($connect, $select_query);
 
-    while ($row = mysqli_fetch_assoc($result_query)) {
-        $product_id = $row['product_id'];
-        $product_name = $row['product_name'];
-        $product_description = $row['product_description'];
-        $product_image1 = $row['product_image1'];
-        $product_image2 = $row['product_image2'];
-        $product_image3 = $row['product_image3'];
-        $product_price = $row['product_price'];
-        $category_id = $row['category_id'];
-        $brand_id = $row['brands_id'];
+function getProducts(){
+
+    global $connect;
+    $selectQuery = "SELECT * FROM `products` order by rand() LIMIT 0,9";
+    $resultQuery = mysqli_query($connect, $selectQuery);
+
+    while ($row = mysqli_fetch_assoc($resultQuery)) {
+        $productId = $row['product_id'];
+        $productName = $row['product_name'];
+        $productDescription = $row['product_description'];
+        $productImage1 = $row['product_image1'];
+        $productImage2 = $row['product_image2'];
+        $productImage3 = $row['product_image3'];
+        $productPrice = $row['product_price'];
+        $categoryId = $row['category_id'];
+        $brandId = $row['brands_id'];
 
         echo "
         <div class='col-md-4 mb-2'>
             <div class='card'>
-                <img src='./admin_section/product_images/$product_image1' class='card-img-top' alt='$product_name'>
+                <img src='./admin_section/product_images/$productImage1' class='card-img-top' alt='$productName'>
                 <div class='card-body'>
-                    <h5 class='card-title'>$product_name</h5>
-                    <p class='card-text'>$product_description</p>
+                    <h5 class='card-title'>$productName</h5>
+                    <p class='card-text'>$productDescription</p>
                     <a href='#' class='btn btn-info'>Add to cart</a>
                     <a href='#' class='btn btn-secondary'>View More</a>
                 </div>
@@ -31,3 +32,19 @@ function getProducts()
         </div>";
     }
 }
+
+function getBrands(){
+    global $connect;
+    $selectBrands = "SELECT * FROM `brands`";
+    $resultBrands = mysqli_query($connect, $selectBrands);
+
+    while($rowData = mysqli_fetch_assoc($resultBrands)){
+        $brandTitle = $rowData['brand_title'];
+        $brandId = $rowData['brands_id'];
+        echo "
+        <li class='nav-item'>
+            <a href='index.php?brand=$brandId' class='nav-link text-light'>$brandTitle</a>
+        </li>";
+    }
+}
+?>
