470ae5a5aee96521f6d71a5318e5c77a9f5a793a
Lawrence <lawrencehome21@gmail.com>
2023-08-12 20:49:55 -0700

    getUniqueBrands function

--- functions/functionsToUse.php
+++ functions/functionsToUse.php
@@ -44,7 +44,7 @@ function getUniqueCategories()
 {
 
     global $connect;
-    // condition to check isset or not
+    // condition to check isset 
     if (isset($_GET['category'])) {
         $categoryId = $_GET['category'];
         $selectQuery = "SELECT * FROM `products` WHERE category_id=$categoryId";
@@ -83,6 +83,49 @@ function getUniqueCategories()
     }
 }
 
+// getting unique brands
+function getUniqueBrands()
+{
+
+    global $connect;
+    // condition to check isset 
+    if (isset($_GET['category'])) {
+        $categoryId = $_GET['category'];
+        $selectQuery = "SELECT * FROM `products` WHERE category_id=$categoryId";
+        $resultQuery = mysqli_query($connect, $selectQuery);
+
+        $number_of_rows = mysqli_num_rows($resultQuery);
+        if($number_of_rows==0)
+        {
+            echo "<h2 class='text-center text-danger'>No stock for these category</h2>";
+        }
+
+        while ($row = mysqli_fetch_assoc($resultQuery)) {
+            $productId = $row['product_id'];
+            $productName = $row['product_name'];
+            $productDescription = $row['product_description'];
+            $productImage1 = $row['product_image1'];
+            $productImage2 = $row['product_image2'];
+            $productImage3 = $row['product_image3'];
+            $productPrice = $row['product_price'];
+            $categoryId = $row['category_id'];
+            $brandId = $row['brands_id'];
+
+            echo "
+            <div class='col-md-4 mb-2'>
+                <div class='card'>
+                    <img src='./admin_section/product_images/$productImage1' class='card-img-top' alt='$productName'>
+                    <div class='card-body'>
+                        <h5 class='card-title'>$productName</h5>
+                        <p class='card-text'>$productDescription</p>
+                        <a href='#' class='btn btn-info'>Add to cart</a>
+                        <a href='#' class='btn btn-secondary'>View More</a>
+                    </div>
+                </div>
+            </div>";
+        }
+    }
+}
 
 
 function getBrands()
