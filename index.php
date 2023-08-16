2a2109c7f251b2f87857cd52dacc291a1a37d9a7
Lawrence <lawrencehome21@gmail.com>
2023-08-16 08:55:07 -0700

    new file display.php

--- index.php
+++ index.php
@@ -31,10 +31,10 @@ include('./functions/functionsToUse.php');
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Products</a>
+                            <a class="nav-link" href="display.php">Products</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Register</a>
