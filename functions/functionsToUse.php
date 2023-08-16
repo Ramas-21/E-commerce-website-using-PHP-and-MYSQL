500b18d11e69278c6e7b07662d71d99bcafafc43
Lawrence <lawrencehome21@gmail.com>
2023-08-15 17:18:05 -0700

    removed isset function in the search_products.php

--- functions/functionsToUse.php
+++ functions/functionsToUse.php
@@ -51,8 +51,7 @@ function getUniqueCategories()
         $resultQuery = mysqli_query($connect, $selectQuery);
 
         $number_of_rows = mysqli_num_rows($resultQuery);
-        if($number_of_rows==0)
-        {
+        if ($number_of_rows == 0) {
             echo "<h2 class='text-center text-danger'>No stock for these category</h2>";
         }
 
@@ -95,8 +94,7 @@ function getUniqueBrands()
         $resultQuery = mysqli_query($connect, $selectQuery);
 
         $number_of_rows = mysqli_num_rows($resultQuery);
-        if($number_of_rows==0)
-        {
+        if ($number_of_rows == 0) {
             echo "<h2 class='text-center text-danger'>These brand is not available</h2>";
         }
 
@@ -165,36 +163,32 @@ function getCategories()
 function searchProducts()
 {
     global $connect;
-    // condition to check isset or not
-    if (!isset($_GET['category'])) {
-        if (!isset($_GET['brand'])) {
-            $selectQuery = "SELECT * FROM `products` order by rand() LIMIT 0,9";
-            $resultQuery = mysqli_query($connect, $selectQuery);
 
-            while ($row = mysqli_fetch_assoc($resultQuery)) {
-                $productId = $row['product_id'];
-                $productName = $row['product_name'];
-                $productDescription = $row['product_description'];
-                $productImage1 = $row['product_image1'];
-                $productImage2 = $row['product_image2'];
-                $productImage3 = $row['product_image3'];
-                $productPrice = $row['product_price'];
-                $categoryId = $row['category_id'];
-                $brandId = $row['brands_id'];
+    $selectQuery = "SELECT * FROM `products` order by rand() LIMIT 0,9";
+    $resultQuery = mysqli_query($connect, $selectQuery);
 
-                echo "
-                <div class='col-md-4 mb-2'>
-                    <div class='card'>
-                        <img src='./admin_section/product_images/$productImage1' class='card-img-top' alt='$productName'>
-                        <div class='card-body'>
-                            <h5 class='card-title'>$productName</h5>
-                            <p class='card-text'>$productDescription</p>
-                            <a href='#' class='btn btn-info'>Add to cart</a>
-                            <a href='#' class='btn btn-secondary'>View More</a>
-                        </div>
-                    </div>
-                </div>";
-            }
-        }
+    while ($row = mysqli_fetch_assoc($resultQuery)) {
+        $productId = $row['product_id'];
+        $productName = $row['product_name'];
+        $productDescription = $row['product_description'];
+        $productImage1 = $row['product_image1'];
+        $productImage2 = $row['product_image2'];
+        $productImage3 = $row['product_image3'];
+        $productPrice = $row['product_price'];
+        $categoryId = $row['category_id'];
+        $brandId = $row['brands_id'];
+
+        echo "
+        <div class='col-md-4 mb-2'>
+            <div class='card'>
+                <img src='./admin_section/product_images/$productImage1' class='card-img-top' alt='$productName'>
+                <div class='card-body'>
+                    <h5 class='card-title'>$productName</h5>
+                    <p class='card-text'>$productDescription</p>
+                    <a href='#' class='btn btn-info'>Add to cart</a>
+                    <a href='#' class='btn btn-secondary'>View More</a>
+                </div>
+            </div>
+        </div>";
     }
 }
