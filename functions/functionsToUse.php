e23fc78852276c9be51d86f43e3e0a24d18a4dcb
Lawrence <lawrencehome21@gmail.com>
2023-08-23 15:27:04 -0700

    checked if the product_id isset()

--- functions/functionsToUse.php
+++ functions/functionsToUse.php
@@ -239,6 +239,41 @@ function searchProducts()
 }
 
 // view more details function
-function view_more_details(){
+function view_more_details()
+{
+    global $connect;
+    // condition to check isset or not
+    if (isset($_GET['product_id'])) {
+        if (!isset($_GET['category'])) {
+            if (!isset($_GET['brand'])) {
+                $selectQuery = "SELECT * FROM `products` order by rand() LIMIT 0,9";
+                $resultQuery = mysqli_query($connect, $selectQuery);
 
+                while ($row = mysqli_fetch_assoc($resultQuery)) {
+                    $productId = $row['product_id'];
+                    $productName = $row['product_name'];
+                    $productDescription = $row['product_description'];
+                    $productImage1 = $row['product_image1'];
+                    $productImage2 = $row['product_image2'];
+                    $productImage3 = $row['product_image3'];
+                    $productPrice = $row['product_price'];
+                    $categoryId = $row['category_id'];
+                    $brandId = $row['brands_id'];
+
+                    echo "
+                <div class='col-md-4 mb-2'>
+                    <div class='card'>
+                        <img src='./admin_section/product_images/$productImage1' class='card-img-top' alt='$productName'>
+                        <div class='card-body'>
+                            <h5 class='card-title'>$productName</h5>
+                            <p class='card-text'>$productDescription</p>
+                            <a href='#' class='btn btn-info'>Add to cart</a>
+                            <a href='product_details.php?product_id=$productId' class='btn btn-secondary'>View More</a>
+                        </div>
+                    </div>
+                </div>";
+                }
+            }
+        }
+    }
 }
