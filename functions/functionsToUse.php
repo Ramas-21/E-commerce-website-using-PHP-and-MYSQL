087f324939c82d7538da74893521793a2965a736
Lawrence <lawrencehome21@gmail.com>
2023-08-24 20:01:57 -0700

    IF it is from the remote address

--- functions/functionsToUse.php
+++ functions/functionsToUse.php
@@ -302,4 +302,8 @@ function getIpAddress()
     elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
         $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
     }
+    else{
+        // whether ip is from the remote address
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
 }
