5badd0a15c96bd368ad0dc4ada4536ff701af975
Lawrence <lawrencehome21@gmail.com>
2023-08-26 19:23:41 -0700

    successful message to be displayed

--- functions/functionsToUse.php
+++ functions/functionsToUse.php
@@ -324,6 +324,7 @@ function cart(){
         {
             $insert_query = "INSERT INTO `cart_details` (product_id,ip_address,quantity) VALUES ($get_product_id,'$ip',0)";
             $resultQuery = mysqli_query($connect, $insert_query);
+            echo "<script>alert('Successfully added to cart.')</script>";
             echo "<script>window.open('index.php','_self')</script>";
         }
     }
