19218921daa4d9f958ec6407bb2738268aeaa7ce
Lawrence <lawrencehome21@gmail.com>
2023-08-30 20:05:11 -0700

    added some margin

--- cart.php
+++ cart.php
@@ -99,7 +99,7 @@ include('./functions/functionsToUse.php');
                 <div class="d-flex mb-5">
                     <h4 class="px-3">Subtotal:<strong class="text-info">5000/-</strong></h4>
                     <a href="index.php"><button class="bg-info px-3 py-2 border-0 mx-3">Continue shopping</button></a>
-                    <a href="#"><button class="bg-secondary p-3 py-2 border-0">Checkout</button></a>
+                    <a href="#"><button class="bg-secondary p-3 py-2 border-0 text-light">Checkout</button></a>
                 </div>
             </div>
         </div>
