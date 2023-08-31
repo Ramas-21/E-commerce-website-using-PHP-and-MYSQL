7285e4e9ca5f92556879055429f9c228dd0fba97
Lawrence <lawrencehome21@gmail.com>
2023-08-30 20:04:08 -0700

    added some padding

--- cart.php
+++ cart.php
@@ -96,10 +96,10 @@ include('./functions/functionsToUse.php');
                         </tr>
                     </tbody>
                 </table>
-                <div class="d-flex">
+                <div class="d-flex mb-5">
                     <h4 class="px-3">Subtotal:<strong class="text-info">5000/-</strong></h4>
-                    <a href="index.php"><button class="bg-info px-3 border-0">Continue shopping</button></a>
-                    <a href="#"><button class="bg-secondary px-3 border-0">Checkout</button></a>
+                    <a href="index.php"><button class="bg-info px-3 py-2 border-0 mx-3">Continue shopping</button></a>
+                    <a href="#"><button class="bg-secondary p-3 py-2 border-0">Checkout</button></a>
                 </div>
             </div>
         </div>
