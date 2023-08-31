72c17bddaeaa9de063c8e9516a144283ca23450e
Lawrence <lawrencehome21@gmail.com>
2023-08-30 19:29:25 -0700

    new file cart.php

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ include('./functions/functionsToUse.php');
                             <a class="nav-link" href="#">Contact</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#"><i class="fa-solid fa-cart-shopping"></i><sup><?php numberOfItemsInCart();?></sup></a>
+                            <a class="nav-link" href="cart.php"><i class="fa-solid fa-cart-shopping"></i><sup><?php numberOfItemsInCart();?></sup></a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Total Price: <?php totalPrice();?>/-</a>
