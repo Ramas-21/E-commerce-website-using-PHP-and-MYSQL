f57d45bd2ce9d4357b9c9cb44e357373511bf08a
Lawrence <lawrencehome21@gmail.com>
2023-09-11 21:01:49 -0700

    echoed the total_price

--- cart.php
+++ cart.php
@@ -129,7 +129,7 @@ include('./functions/functionsToUse.php');
                         </tbody>
                     </table>
                     <div class="d-flex mb-5">
-                        <h4 class="px-3">Subtotal:<strong class="text-info">5000/-</strong></h4>
+                        <h4 class="px-3">Subtotal:<strong class="text-info"><?php echo $total_price?>/-</strong></h4>
                         <a href="index.php"><button class="bg-info px-3 py-2 border-0 mx-3">Continue shopping</button></a>
                         <a href="#"><button class="bg-secondary p-3 py-2 border-0 text-light">Checkout</button></a>
                     </div>
