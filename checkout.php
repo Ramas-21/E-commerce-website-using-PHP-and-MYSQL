bc1e26d2e263f3b52ddb40ee490f78eaea7616db
Lawrence <lawrencehome21@gmail.com>
2023-09-16 11:40:48 -0700

    removed the cart icon and total price from the navbar

--- checkout.php
+++ checkout.php
@@ -9,7 +9,7 @@ include('includes/connect.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>E-commerce website</title>
+    <title>Checkout page</title>
     <!------- Bootstrap css links ---->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!------- font awesome link ------>
@@ -40,12 +40,6 @@ include('includes/connect.php');
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Contact</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="cart.php"><i class="fa-solid fa-cart-shopping"></i><sup><?php numberOfItemsInCart();?></sup></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Total Price: <?php totalPrice();?>/-</a>
                         </li>
                     </ul>
                     <form class="d-flex" role="search" action="search_products.php" method="get">
