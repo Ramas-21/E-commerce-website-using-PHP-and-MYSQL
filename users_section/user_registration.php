715afd83e6094b744907b9e61e6c26d8e03f7761
Lawrence <lawrencehome21@gmail.com>
2023-09-17 16:55:39 -0700

    input field for password

--- users_section/user_registration.php
+++ users_section/user_registration.php
@@ -29,8 +29,8 @@
                         <input type="file" id="user_image" class="form-control" required name="user_image">
                     </div>
                     <div class="form-outline">
-                        <label for="user_username" class="form-label">Username</label>
-                        <input type="text" id="user_username" class="form-control" placeholder="Enter your username" autocomplete="off" required name="user_username">
+                        <label for="password" class="form-label">Pass7</label>
+                        <input type="password" id="password" class="form-control" placeholder="Enter your password" autocomplete="off" required name="password">
                     </div>
                     <div class="form-outline">
                         <label for="user_username" class="form-label">Username</label>
