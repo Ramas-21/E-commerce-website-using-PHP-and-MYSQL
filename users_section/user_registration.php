c0a830ac076e1ab2f0bbbb75d6bcc392850674c8
Lawrence <lawrencehome21@gmail.com>
2023-09-20 20:24:35 -0700

    else statement to redirect to index.php

--- users_section/user_registration.php
+++ users_section/user_registration.php
@@ -98,5 +98,8 @@ if (isset($_POST['user_register'])) {
         echo "<script>alert('You have items in your cart.')</script>";
         echo "<script>window.open('checkout.php','_self')</script>";
     }
+    else{
+        echo "<script>window.open('../index.php','_self')</script>";
+    }
 }
 ?>
