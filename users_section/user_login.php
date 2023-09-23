c1d8166706dcb35169a3e2d0eca9b0222867ad48
Lawrence <lawrencehome21@gmail.com>
2023-09-22 20:31:25 -0700

    removed the font awesome link

--- users_section/user_login.php
+++ users_section/user_login.php
@@ -9,7 +9,6 @@ include('../includes/connect.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!------- Bootstrap css links ---->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <!------- font awesome link ------>
     <title>User login</title>
 </head>
 
