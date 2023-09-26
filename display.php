b391307452b06ecb6b422e6ed420044c707d1802
Lawrence <lawrencehome21@gmail.com>
2023-09-25 20:04:03 -0700

    included logout button

--- display.php
+++ display.php
@@ -1,6 +1,7 @@
 <?php
 include('includes/connect.php');
 include('./functions/functionsToUse.php');
+session_start();
 ?>
 
 <!DOCTYPE html>
@@ -64,9 +65,18 @@ include('./functions/functionsToUse.php');
                 <li class="nav-item">
                     <a href="#" class="nav-link">Welcome Guest</a>
                 </li>
-                <li class="nav-item"></li>
-                <a href="./users_section/user_login.php" class="nav-link">Login</a>
-                </li>
+                <?php
+                if(!isset($_SESSION['username'])){
+                    echo "<li class='nav-item'></li>
+                    <a href='./users_section/user_login.php' class='nav-link'>Login</a>
+                    </li>";
+                }
+                else{
+                    echo "<li class='nav-item'></li>
+                    <a href='logout.php' class='nav-link'>Logout</a>
+                    </li>";
+                }
+                ?>
             </ul>
         </nav>
 
